5cc0615dc149720b24d6c29e299e48f637814bce
petr0n <petron@gmail.com>
2019-04-13 13:30:32 -0500

    Lots of dev progress

---
+++ index.js
@@ -0,0 +1,137 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+let chalk = require('chalk');
+let Table = require('cli-table');
+
+console.log('Welcome to BamaZon - the BtoZ store!');
+
+
+// app files
+let product = require('./product');
+
+// db connection 
+let conn = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'o7kLUrUb18gdzQzu',
+    database: 'bamazon'
+});
+
+conn.connect(function (err) {
+    if (err) throw err;
+    initStore();
+});
+
+
+function initStore() {
+    getProducts();
+}
+
+function getProducts() {
+    let table = new Table({
+        head: [
+            'ProductId',
+            'Product Name',
+            'Dept',
+            'Price',
+            'Qty'],
+        // colWidths: [100, 200, 100, 100, 100]
+    });
+    let query = 'SELECT * FROM products';
+    conn.query(query, function (err, res) {
+        if (err) { console.log(err); }
+        res.map(function(row) {
+            let productRow = [row.product_id, row.product_name, row.department_name, '$' + row.sale_price, row.stock_quantity];
+            table.push(productRow);
+        });
+        console.log(table.toString());
+        whatProduct();
+    });
+}
+
+
+let getProduct = function(productId) {
+    return new Promise(function(resolve, reject) {
+        if (!isNaN(productId)) {
+            let query = 'SELECT * FROM products WHERE ?';
+            conn.query(query, {product_id: productId}, function (err, res) {
+                if (err) { console.log(err); }
+                // console.log(res);
+                if (res.length) {
+                    let product = [
+                        res[0].product_id, 
+                        res[0].product_name, 
+                        res[0].department_name, 
+                        res[0].sale_price, 
+                        res[0].stock_quantity
+                    ];
+                    resolve(product);
+                } else {
+                    reject('product is not found');
+                }
+            });
+        } else {
+            reject('invalid product id');
+        }
+    });
+}
+
+
+function whatProduct(){
+    console.log('What would you like to buy?');
+    inquirer.prompt([
+        {
+            name: "productId",
+            type: "input",
+            message: "Product Id?",
+            validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }])
+    .then(function(answer) {
+        getProduct(answer.productId)
+            .then(function(productArr){
+                console.log('You chose: ' + productArr[1]);
+                // console.log(productArr);
+                whatQty(productArr);
+            })
+            .catch(function(err){
+                console.log('Error: ' + err);
+                whatProduct();
+            });
+    });
+}
+
+function whatQty(productArr){
+    console.log('What quantity do you to buy?');
+    inquirer.prompt([
+        {
+            name: "qty",
+            type: "input",
+            message: "Stock Quantity?",
+            validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }])
+    .then(function(answer) {
+        let totalPrice = productArr[3] * answer.qty;
+        let remainingQty = productArr[4] - answer.qty;
+        // console.log(productArr[4] - answer.qty);
+        if (remainingQty > 0) {
+            // success
+            // runStore();
+            console.log('Success! You have purchased ' + answer.qty + ' ' + productArr[1] + 's for a total price of ' + totalPrice.toFixed(2) );
+            initStore();
+        } else {
+            console.log('Not enough available. Try again.');
+            whatQty(productArr)
+        }
+    });
+}
